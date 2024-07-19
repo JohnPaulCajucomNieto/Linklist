c6ad976b4b74a9b9b4e5bfd90e997a35077a61d1
JohnPaulCajucomNieto <91181225+JohnPaulCajucomNieto@users.noreply.github.com>
2024-07-19 18:44:52 +0800

    Add files via upload

---
+++ list/src/javafundamentals/ascending.java
@@ -0,0 +1,14 @@
+package javafundamentals;
+
+public class ascending and decending {
+
+	public static void main(String[] args) {
+	 
+    string name, course, major;
+    
+    System.out.println("Enter your name: ")
+	System.out.println("Enter your course: ");
+    
+	}
+
+}
